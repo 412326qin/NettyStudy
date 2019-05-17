1857039797ad8621999a8d1b4c9392494f5f643e
秦子尧 <159203067@qq.com>
2019-05-18 00:37:46 +0800

    commit

---
+++ src/main/java/com/mashibing/io/nio/CharsetTest.java
@@ -0,0 +1,50 @@
+package com.mashibing.io.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+public class CharsetTest {
+
+	public static void main(String[] args) {
+		Map<String, Charset> map = Charset.availableCharsets();
+		for (Entry<String, Charset> charset : map.entrySet()) {
+			System.out.println(charset.getKey()+"="+charset.getValue());
+		}
+	}
+	@Test
+	public void test(){
+		System.out.println("CharsetTest.test()");
+	}
+	@Test
+	public void charName() throws Exception{
+		Charset cs1 = Charset.forName("gbk");
+		CharsetEncoder encode = cs1.newEncoder();
+		CharsetDecoder decode = cs1.newDecoder();
+		CharBuffer cBuf = CharBuffer.allocate(20);
+		cBuf.put("啊的发电啊多发点阿斯蒂芬啊打发士大夫");
+		cBuf.flip();
+		ByteBuffer bBuf = encode.encode(cBuf);
+		for (int i = 0; i < 10; i++) {
+			System.out.println(bBuf.get());
+		}
+		bBuf.flip();
+		CharBuffer cBuf2 = decode.decode(bBuf);
+		System.out.println(cBuf2.toString());
+		System.out.println("***********");
+		Charset charset = Charset.forName("UTF-8");
+		CharsetDecoder decode1 = charset.newDecoder();
+		bBuf.flip();
+		//如果用CharsetDecoder的decode方法出现半个中文，
+		//报java.nio.charset.MalformedInputException: Input length = 1异常
+		//可以使用Charset.decode()方法，会出现乱码，但不报异常
+		CharBuffer cBuf3 = decode1.decode(bBuf);
+		System.out.println(cBuf3.toString());
+	}
+}
